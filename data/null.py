8d6b60f6463b7de81fdae3c0e01d4df8e6e7acd0
Devanshu Haldar <devanshuhaldar@rift-97.dynamic2.rpi.edu>
2022-10-18 19:53:36 -0400

    added power requirement per cryptonetwork

--- data/null.py
+++ data/null.py
@@ -45,12 +45,57 @@ def average_market_price(df): # returns map of average market price for years 20
         averages[key] = avg
     return averages
 
-def show_average_hash_rate(year,hashrate):
-    plt.bar(year,hashrate, label = 'Hash-Rate')
-    plt.title('Hashrate Avg per Year')
-    plt.xlabel('Year')
-    plt.ylabel('Hash Rate (100 million)')
-    plt.show()
+def plot_hash_rate(df):
+    year = df['timestamp'].to_numpy()
+    year = [dt.datetime(int(yr[:4]), int(yr[5:7]), int(yr[8:10])) for yr in year] #removing timestamps and only having dates
+    hashrate = df['hash-rate'].to_numpy()
+    plt.plot(year, hashrate, label = "Hash-Rate")
+    plt.title("hash rate vs year all time")
+    plt.xlabel("year")
+    plt.ylabel("hashrate")
+   # plt.show()
+
+
+def plot_market_price(df):
+    year = df['timestamp'].to_numpy()
+    year = [dt.datetime(int(yr[:4]), int(yr[5:7]), int(yr[8:10])) for yr in year] #removing timestamps and only having dates
+    hashrate = df['market-price'].to_numpy()
+    plt.plot(year, hashrate, label = "market-price")
+    plt.title("market price vs year all time")
+    plt.xlabel("year")
+    plt.ylabel("marketp rice")
+   # plt.show()
+    
+def plot_power_req_per_cryptonetwork(df):
+    # P (power requirement per cryptonetwork) = HR(hashes/second) x PE(Joules/ Hash) x 2.78x10^{-10} x 3600
+    # using power efficiency of a mining machine in 2017
+
+    m = average_hash_rates(df)
+    years, averages = [], []
+    for yr, avg in m.items():
+         years.append(yr)
+         averages.append(avg)
+    
+    x = .15 * 2.78 * 10**(-10) * 3600
+    averages = [avg * x for avg in averages]
+    plt.bar(years, averages, label = 'power required')
+    plt.title('power for cryptonetwork by year')
+    plt.xlabel('year')
+    plt.ylabel('power (megawatts)')
+
+    
+
+    
+    # year = df['timestamp'].to_numpy()
+    # year = [dt.datetime(int(yr[:4]), int(yr[5:7]), int(yr[8:10])) for yr in year] #removing timestamps and only having dates
+    # hashrate = df['hash-rate'].to_numpy()
+    #implement above equation for Pß
+    # x = .15 * 2.78 * 10**(-10) * 3600 #2017 power efficiency included in equation
+    # power = [hr * x for hr in hashrate]
+    # plt.plot(year, power, label = "power required")
+    # plt.title("power required")
+    # plt.xlabel("year")
+    # plt.ylabel("power")
         
         
 
@@ -59,7 +104,7 @@ def show_average_hash_rate(year,hashrate):
 if __name__ == "__main__": 
     all_time_hash_rates_df = pd.read_csv("/Users/devanshuhaldar/Desktop/RPI/Blockchain-Energy-Labels/bitcoin/data/bitcoin-all-time-hash-rate.csv")
     all_time_market_price_df = pd.read_csv("/Users/devanshuhaldar/Desktop/RPI/Blockchain-Energy-Labels/bitcoin/data/bitcoin-all-time-market-price.csv")
-
+    
 
     hash_rate_averages = average_hash_rates(all_time_hash_rates_df)
     market_prices = average_market_price(all_time_market_price_df)
@@ -72,26 +117,27 @@ if __name__ == "__main__":
     # for key,val in market_prices.items():
     #     print("Year",key, " --> ",val)
 
-    #plot average hash rates by year 2009 - 2022
-    # year = []
-    # hashrate = []
-    # for yr, hr in hash_rate_averages.items():
-    #     year.append(yr)
-    #     hashrate.append(hr)
-    # show_average_hash_rate(year,hashrate)
 
-   
-    year = all_time_hash_rates_df['timestamp'].to_numpy()
-    year = [dt.datetime(int(yr[:4]), int(yr[5:7]), int(yr[8:10])) for yr in year] #removing timestamps and only having dates
-    hashrate = all_time_hash_rates_df['hash-rate'].to_numpy()
-    plt.plot(year, hashrate, label = "Hash-Rate")
-    plt.title("hash rate vs year all time")
-    plt.xlabel("year")
-    plt.ylabel("hashrate")
+
+
+    #plots: 
+
+    # #plot average hash rates 2009 - 2022
+    # plot_hash_rate(all_time_hash_rates_df)
+    # plt.show()
+    # #plot market price for years 2009 - 2022
+    # plot_market_price(all_time_market_price_df)
+    # plt.show()
+
+
+    
+    # 2017-2018 .015 JH^-1 
+
+    # P (power requirement per cryptonetwork) = HR(hashes/second) x PE(Joules/ Hash) x 2.78x10^{-10} x 3600 
+    # ends up being the same because multiplied by constants --> I could multiply power efficiency with average per each year. 
+    plot_power_req_per_cryptonetwork(all_time_hash_rates_df)
     plt.show()
-
-
-
+    #  Network velocity ($ Us per hour)
 
 
 
